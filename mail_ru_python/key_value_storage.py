f6193108b118477e491189989f49d364d9cf2e71
P-Shumskiy <fwshumskiy@gmail.com>
2020-01-21 22:16:22 +0300

    key_value_storage.py done! TODO refactor in functional style

--- mail_ru_python/key_value_storage.py
+++ mail_ru_python/key_value_storage.py
@@ -1,13 +1,35 @@
 import argparse
+import json
+import os
+import tempfile
+
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number",
-                    type=int)
-parser.add_argument("-v", "--verbosity", help="increase output verbosity",
-                    action="store_true")
+parser.add_argument("--key", help="take key of storage", type=str)
+parser.add_argument("--val", help="take value of current key", type=str)
 args = parser.parse_args()
-answer = args.square**2
-if args.verbosity:
-    print(f"square of {args.square} equals {answer}")
-else:
-    print(answer)
+
+mode = 'r+' if os.path.exists(storage_path) else 'w+'
+
+with open(storage_path, mode=mode) as f:
+    try:
+        storage_data = json.load(f)
+    except json.decoder.JSONDecodeError:
+        storage_data = dict()
+
+if args.key:
+    if args.val:
+        if storage_data.get(args.key):
+            storage_data[args.key].append(args.val)
+        else:
+            storage_data[args.key] = [args.val]
+    else:
+        if storage_data.get(args.key):
+            print(", ".join(map(str, storage_data[args.key])))
+        else:
+            print('None')
+
+with open(storage_path, 'r+') as f:
+    json.dump(storage_data, f)
