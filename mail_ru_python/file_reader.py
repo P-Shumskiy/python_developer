e9cfac7523acd54de334ce1239253d6855078e70
P-Shumskiy <fwshumskiy@gmail.com>
2020-01-23 23:50:33 +0300

    small visual fixex

--- mail_ru_python/file_reader.py
+++ mail_ru_python/file_reader.py
@@ -2,14 +2,13 @@ from pathlib import Path
 
 
 class FileReader:
-
     def __init__(self, path):
         self.path = Path(path)
 
     def read(self):
         try:
-            with open(self.path, 'r') as f:
+            with open(self.path, "r") as f:
                 content = f.read().strip()
                 return content
         except FileNotFoundError:
-            return ''
+            return ""
