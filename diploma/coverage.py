677ab2705be08244d39d8d4be8eac34842b7fa2c
P-Shumskiy <fwshumskiy@gmail.com>
2020-02-10 18:06:08 +0300

    tech

--- diploma/coverage.py
+++ diploma/coverage.py
@@ -42,6 +42,26 @@ def time_check(func):
     return wrapper
 
 
+def breadth_calculation_in_target_regions(target_regions: str = target_regions_,
+                                          patient: str = sample) -> pd.DataFrame:  # TODO need to be refactored as
+    command = f"samtools coverage -a {patient} -b {target_regions}".strip().split(' ')
+    temp_file = tempfile.NamedTemporaryFile()
+
+    samtools_coverage = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+
+    with open(temp_file.name, 'w')as temp:
+        temp.write(samtools_coverage.stdout)
+
+    if not samtools_coverage.stderr:
+        names = ["chr", "start", "end", "gene", "to_drop", "starnd", "n_reads",
+                 "n_bases_covered", "length_of_gene_fragment", "percent_covered"]
+        df = pd.read_csv(temp_file, sep="\t", names=names).drop("to_drop", axis=1)
+
+        return df
+
+    raise PipelineError
+
+
 def depth_calculation_in_target_regions(target_regions: str = target_regions_,
                                         patient: str = sample) -> pd.DataFrame:
     command = f"samtools bedcov {target_regions} {patient}".strip().split(' ')
@@ -60,6 +80,22 @@ def depth_calculation_in_target_regions(target_regions: str = target_regions_,
 
     raise PipelineError(
         f"an error occurred during the\n >>>> {' '.join(command)} <<<<\nERROR from {samtools_bedcov.stderr}")
+
+
+def breadth_calculation_for_genes(df):
+    cover = {"gene": [],
+             "percent_covered": []}
+
+    for gene in df.gene.unique():
+        percent = df[df.gene == gene].percent_covered.mean()
+        cover["gene"].append(gene)
+        cover["percent_covered"].append(round(percent * 100, 2))
+
+    df = pd.DataFrame(cover).sort_values(by="percent_covered", ascending=False) \
+        .reset_index() \
+        .drop("index", axis=1)
+
+    return df
 
 
 def depth_calculation_for_genes(df):
@@ -84,12 +120,20 @@ def depth_calculation_for_genes(df):
 
 @time_check
 def main():
-    df = depth_calculation_in_target_regions()
-    df = depth_calculation_for_genes(df=df)
-    df.to_excel(f"./gene_coverage/{save_name}.xlsx", sheet_name=f"{save_name}", index=False)
+    depth = depth_calculation_in_target_regions()
+    depth = depth_calculation_for_genes(df=depth)
+
+    breadth = breadth_calculation_in_target_regions()
+    breadth = breadth_calculation_for_genes(df=breadth)
+
+    # result = depth.join(on=depth, how=inner)
+
+    depth.to_excel(f"./gene_coverage/depth.{save_name}.xlsx", sheet_name=f"{save_name}", index=False)
+    print(f"saved to {os.getcwd()}/gene_coverage/{save_name}.xlsx")
+    breadth.to_excel(f"./gene_coverage/breadth.{save_name}.xlsx", sheet_name=f"{save_name}", index=False)
     print(f"saved to {os.getcwd()}/gene_coverage/{save_name}.xlsx")
 
-    barplot_for_genes = sns.barplot(df.gene, df.mean_cov_depth_X)  # TODO pretty barplot
+    barplot_for_genes = sns.barplot(depth.gene, depth.mean_cov_depth_X)  # TODO pretty barplot
     fig = barplot_for_genes.get_figure()
     fig.savefig(f"figures/{save_name}.png")
     print(f"saved to {os.getcwd()}/figures/{save_name}.png")
